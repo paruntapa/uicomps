77ac2d5b218cc19f631d49df3636458f374f998f
paruntapa <geniustechmax@gmail.com>
2025-11-27 23:58:43 +0530

    html done css half

--- src/app/sliding-stack/page.tsx
+++ src/app/sliding-stack/page.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import './sliding-stack.css'
 
 const slidingStack = () => {
   return (
-    <div>slidingStack</div>
+    <div>
+      <nav>
+        <div className='nav-logo'>
+          <a href="#"> Sliding Stack</a>
+        </div>
+
+        <div className='nav-links'>
+          <a href="#">BookHook</a>
+          <a href="#">Vertical Series</a>
+          <a href="#">LIT STUDIO</a>
+          <a href="#">Sampark</a>
+        </div>
+
+        <div className="nav-cta">
+          <a href="#">Join De Gang</a>
+        </div>
+      </nav>
+
+      <footer>
+        <p>
+          Experiment 6028 / Brute_Powered Slider
+        </p>
+        <p>
+          Developed by withoutme
+        </p>
+      </footer>
+
+      <div className="container">
+        <div className="slider">
+
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
