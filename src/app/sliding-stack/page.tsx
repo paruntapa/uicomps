bfde8f7daf1893e28570d74180559d4712663140
paruntapa <geniustechmax@gmail.com>
2025-12-10 01:24:35 +0530

    sliding-stack added

--- src/app/sliding-stack/page.tsx
+++ src/app/sliding-stack/page.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
-import './sliding-stack.css'
+'use client'
+
+import React, { useEffect } from 'react';
+import './sliding-stack.css';
+import gsap from 'gsap';
+import SplitText from 'gsap/SplitText';
+
+interface slideType {
+  title: string,
+  image: string
+}
 
 const slidingStack = () => {
+
+  const slideData: slideType[] = [
+    {title: "gojo",  image: ''},
+    {title: "izuku", image: ''},
+    {title: "anya", image: ''},
+    {title: "saitama", image: ''},
+    {title: "l", image: ''}
+  ]
+
+  useEffect(() => {
+    gsap.registerPlugin(SplitText);
+
+    const container = document.querySelector('.container');
+    const slider = document.querySelector('.slider');
+
+    let frontSlideIndex = 0;
+    let isSliderAnimating = false;
+
+    const initializeSlider = () => {
+      slideData.forEach((data, index) => {
+        const slide = document.createElement('div');
+
+        slide.classList = 'slide';
+        slide.innerHTML = `
+                   <img 
+                   src="${data.image}"
+                   alt="${data.title}"
+                   />
+                   <h1 class="slide-title">${data.title}</h1>
+        `;
+
+        slider?.appendChild(slide);
+      });
+
+      let slides = document.querySelector('.slide');
+
+    }
+
+  
+  }, [])
+  
   return (
     <div>
       <nav>
